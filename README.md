25620407caf8d92b4a5bc71202635d56d2869267
Artur Killdragon <artur.guitelar@gmail.com>
2019-04-09 15:41:33 -0300

    Colocando eventos nos botões.

--- README.md
+++ README.md
@@ -4,5 +4,17 @@
 
 Calculadora desenvolvida como exemplo do Curso Completo de JavaScript na Udemy.com.
 
+Utiliza recursos atuais do Javascript a partir do ES6.
+
+Conceitos abordados nas aulas deste projeto:
+- OOP
+- MVC
+
+Idéias interessantes aprendidas nas aulas:
+- Utilizando classe com construtor, getters e setters.
+- Pegando elementos do dom com querySelector e querySelectorAll.
+- Utilização e customização da classe Date e dos métodos toLocale.
+- Tratamento de últiplos eventos em um elemento.
+
 ### Projeto
 ![Calculadora](https://firebasestorage.googleapis.com/v0/b/hcode-com-br.appspot.com/o/calculadora-hcode.jpg?alt=media&token=5406aa3f-b965-401c-9b4e-654609c78b33)
--- scripts/controller/CalculatorController.js
+++ scripts/controller/CalculatorController.js
@@ -12,10 +12,11 @@ class CalculatorController
 
         // methods
         this.init();
+        this.initButtonEvents();
     }
 
     init() {       
-        // set e atualiza data e hora da calculadora
+        // seta e atualiza data e hora da calculadora
         this.setDisplayDateTime();
 
         setInterval(()=>{
@@ -58,6 +59,9 @@ class CalculatorController
     /* Getters e Setters - fim */
 
     /* Methods */
+    /**
+     * Mostra data e hora no display.
+     */
     setDisplayDateTime() {
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: '2-digit',
@@ -66,4 +70,36 @@ class CalculatorController
         });
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
+
+    /**
+     * Inicia Eventos nos botões.
+     */
+    initButtonEvents() {
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+
+        // adiciona eventos para cada botão
+        buttons.forEach((button, index) => {
+            this.addEventListenerAll(button, 'click drag', event => {
+                console.log(button.className.baseVal.replace("btn-", ""), index);
+            });
+
+            this.addEventListenerAll(button, 'mouseover mouseup mousedown', event => {
+                button.style.cursor = 'pointer';
+            });
+        });        
+    }
+
+    /**
+     * Possibilita colocar mais de um evento em um elemento.
+     * 
+     * @param element Elemento que receberá os eventos.
+     * @param events String com eventos separados por espaço.
+     * @param fn Função de callback que será executada ao acionar o evento.
+     */
+    addEventListenerAll(element, events, fn) {
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+    }    
+
 }
